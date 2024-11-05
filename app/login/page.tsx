7e2f76aa6cf51cefa8258fc0e4820d802d893cc4
SenaBel <abelsena1@hotmail.com>
2024-11-05 14:07:20 -0300

    fix: adjusting login for smaller screens

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="grid h-full grid-cols-2">
+    <div className="grid h-full grid-cols-1 md:grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
@@ -22,20 +22,20 @@ const LoginPage = async () => {
           height={39}
           className="mb-8"
         />
-        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="mb-8 text-muted-foreground">
+        <h1 className="mb-3 text-3xl font-bold md:text-4xl">Bem-vindo</h1>
+        <p className="mb-8 text-sm text-muted-foreground md:text-base">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
-          <Button variant="outline">
+          <Button variant="outline" className="flex items-center">
             <LogInIcon className="mr-2" />
             Fazer login ou criar conta
           </Button>
         </SignInButton>
       </div>
-      <div className="relative h-full w-full">
+      <div className="relative hidden h-full w-full md:block">
         <Image
           src="/login.png"
           alt="Faça Login"
