cdd9134b5201cf975265a8e49e8cb0ff1857e6d4
SenaBel <abelsena1@hotmail.com>
2024-11-08 13:21:49 -0300

    feat: add card in dashboard

---
+++ app/(home)/_components/summary-card.tsx
@@ -0,0 +1,42 @@
+import AddTransactionButton from "@/app/_components/add-transaction-button";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+
+interface SummaryCardProps {
+  icone: React.ReactNode;
+  title: string;
+  amount: number;
+  size?: "small" | "large";
+}
+
+const SummaryCard = ({
+  icone,
+  title,
+  amount,
+  size = "small",
+}: SummaryCardProps) => {
+  return (
+    <Card className={`${size === "large" ? "bg-white bg-opacity-5" : ""}`}>
+      <CardHeader className="flex-row items-center gap-4">
+        {icone}
+        <p
+          className={`${size === "small" ? "text-muted-foreground" : "text-white opacity-70"}`}
+        >
+          {title}
+        </p>
+      </CardHeader>
+      <CardContent className="flex justify-between">
+        <p
+          className={`font-bold ${size === "small" ? "text-2xl" : "text-4xl"}`}
+        >
+          {Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(amount)}
+        </p>
+        {size === "large" && <AddTransactionButton />}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SummaryCard;
